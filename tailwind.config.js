9b082decbe4cfe2713b2742a5361178f285b07ea
Sajjad Hussain <124031367+techforward009@users.noreply.github.com>
2026-08-01 18:00:13 +0500

    Refactor Tailwind CSS config to CommonJS format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,4 +16,3 @@ const config: Config = {
   },
   plugins: [],
 };
-export default config;
