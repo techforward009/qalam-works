bcbc0de219369a2b9aecb323573a7aca02550850
Sajjad Hussain <124031367+techforward009@users.noreply.github.com>
2026-08-01 17:45:45 +0500

    Refactor tailwind.config.js to TypeScript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class',
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: "class",
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nastaliq: ["var(--font-nastaliq)", "serif"],
+        sans: ["var(--font-inter)", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
+export default config;
